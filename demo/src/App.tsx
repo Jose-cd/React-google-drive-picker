ca008dd5b065c3639bb211ff220b47ed0ac07d94
jose-cd <josesaygg@gmail.com>
2021-05-16 15:00:56 -0500

    dist

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import  { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import useDrivePicker from 'react-google-drive-picker'
