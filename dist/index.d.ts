1f618ccfcc3edbc9af59fd4b742a2952bc5d36c2
José <josesaygg@gmail.com>
2022-05-26 23:05:17 -0500

    deleted dist folder

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { authResult, PickerConfiguration } from './typeDefs';
-export default function useDrivePicker(): [
-    (config: PickerConfiguration) => boolean | undefined,
-    authResult | undefined
-];
-//# sourceMappingURL=index.d.ts.map
--- dist/useInjectScript.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function useInjectScript(): [boolean, boolean];
-//# sourceMappingURL=useInjectScript.d.ts.map
